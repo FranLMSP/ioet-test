f9f982ced887c34afd5911a8b216db8e10d52983
Franco Colmenarez <info@francoacg.com>
2022-01-24 22:57:20 -0500

    Refactor employees structure

--- schedule-coincidences.js
+++ schedule-coincidences.js
@@ -1,42 +1,33 @@
-const getEmployeesList = (textInput) => {
-  let list = [];
-  const lines = textInput.split('\n');
-  for (const line of lines) {
-    if (!line.trim()) continue;
-    const [name, scheduleText] = line.split('=');
-    list.push({
-      name,
-      schedule: getEmployeeSchedule(scheduleText),
-    });
+class Employee {
+  constructor(text) {
+    const [name, scheduleText] = text.split('=');
+    this.name = name;
+    this.schedule = new Schedule(scheduleText);
   }
-
-  return list;
 }
 
-const getEmployeeSchedule = (scheduleText) => {
-  let days = {};
-  if (!scheduleText.trim()) return {};
-  const dayNames = scheduleText.split(',').map((text) => text.substring(0, 2));
-  const hours = scheduleText
-    .split(',')
-    .map((text) => {
-      const [from, to] = text.substring(2).split('-');
-      return {from, to};
-    });
-  for (index in dayNames) {
-    const day = dayNames[index];
-    const hour = hours[index];
-    days[day] = hour;
+class Schedule {
+  constructor(text) {
+    let days = {};
+    if (!text.trim()) {
+      this.days = {};
+      return;
+    }
+    const dayNames = text.split(',').map((s) => s.substring(0, 2));
+    const hours = text
+      .split(',')
+      .map((text) => {
+        const [from, to] = text.substring(2).split('-');
+        return {from, to};
+      });
+    for (const index in dayNames) {
+      const day = dayNames[index];
+      const hour = hours[index];
+      days[day] = hour;
+    }
+
+    this.days = days;
   }
-
-  return days;
-}
-
-const getHourCoincidences = (employee, otherEmployees) => {
-}
-
-const processEmployeesText = (textInput) => {
-  return '';
 }
 
 const hourRangesCoincide = (a, b) => {
@@ -55,8 +46,7 @@ const hourRangesCoincide = (a, b) => {
 }
 
 module.exports = {
-  getEmployeesList,
-  getEmployeeSchedule,
-  getHourCoincidences,
+  Employee,
+  Schedule,
   hourRangesCoincide,
 };
--- schedule-coincidences.test.js
+++ schedule-coincidences.test.js
@@ -1,146 +1,104 @@
 const {
-  getEmployeeSchedule,
-  getEmployeesList,
-  processEmployeesText,
+  Employee,
+  Schedule,
   hourRangesCoincide,
 } = require('./schedule-coincidences');
 
-test('Decode employee schedule text list', () => {
-  expect(getEmployeeSchedule('MO10:15-12:00')).toEqual({
-    'MO': {
-      from: '10:15',
-      to: '12:00',
+test('Decode schedule text list', () => {
+  expect(new Schedule('MO10:15-12:00')).toEqual({
+    days: {
+      'MO': {
+        from: '10:15',
+        to: '12:00',
+      },
     },
   });
 
-  expect(getEmployeeSchedule('MO10:15-12:00,TU10:00-12:00')).toEqual({
-    'MO': {
-      from: '10:15',
-      to: '12:00',
-    },
-    'TU': {
-      from: '10:00',
-      to: '12:00',
+  expect(new Schedule('MO10:15-12:00,TU10:00-12:00')).toEqual({
+    days: {
+      'MO': {
+        from: '10:15',
+        to: '12:00',
+      },
+      'TU': {
+        from: '10:00',
+        to: '12:00',
+      },
     },
   });
 });
 
 test('Decode employees text list', () => {
-  expect(getEmployeesList(
-`RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00
-ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00
-ANDRES=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00
-`
-  )).toEqual([
-    {
+  expect(new Employee('RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00'))
+    .toEqual({
       name: 'RENE',
       schedule: {
-        'MO': {
-          from: '10:00',
-          to: '12:00',
-        },
-        'TU': {
-          from: '10:00',
-          to: '12:00',
-        },
-        'TH': {
-          from: '01:00',
-          to: '03:00',
-        },
-        'SA': {
-          from: '14:00',
-          to: '18:00',
-        },
-        'SU': {
-          from: '20:00',
-          to: '21:00',
+        days: {
+          'MO': {
+            from: '10:00',
+            to: '12:00',
+          },
+          'TU': {
+            from: '10:00',
+            to: '12:00',
+          },
+          'TH': {
+            from: '01:00',
+            to: '03:00',
+          },
+          'SA': {
+            from: '14:00',
+            to: '18:00',
+          },
+          'SU': {
+            from: '20:00',
+            to: '21:00',
+          },
         },
       },
-    },
-    {
+    });
+
+  expect(new Employee('ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00'))
+    .toEqual({
       name: 'ASTRID',
       schedule: {
-        'MO': {
-          from: '10:00',
-          to: '12:00',
-        },
-        'TH': {
-          from: '12:00',
-          to: '14:00',
-        },
-        'SU': {
-          from: '20:00',
-          to: '21:00',
+        days: {
+          'MO': {
+            from: '10:00',
+            to: '12:00',
+          },
+          'TH': {
+            from: '12:00',
+            to: '14:00',
+          },
+          'SU': {
+            from: '20:00',
+            to: '21:00',
+          },
         },
       },
-    },
-    {
+    });
+
+  expect(new Employee('ANDRES=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00'))
+    .toEqual({
       name: 'ANDRES',
       schedule: {
-        'MO': {
-          from: '10:00',
-          to: '12:00',
-        },
-        'TH': {
-          from: '12:00',
-          to: '14:00',
-        },
-        'SU': {
-          from: '20:00',
-          to: '21:00',
-        },
-      },
-    },
-  ]);
-
-  expect(getEmployeesList(
-`RENE=MO10:15-12:00,TU10:00-12:00,TH13:00-13:15,SA14:00-18:00,SU20:00-21:00
-ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00
-`
-  )).toEqual([
-    {
-      name: 'RENE',
-      schedule: {
-        'MO': {
-          from: '10:15',
-          to: '12:00',
-        },
-        'TU': {
-          from: '10:00',
-          to: '12:00',
-        },
-        'TH': {
-          from: '13:00',
-          to: '13:15',
-        },
-        'SA': {
-          from: '14:00',
-          to: '18:00',
-        },
-        'SU': {
-          from: '20:00',
-          to: '21:00',
+        days: {
+          'MO': {
+            from: '10:00',
+            to: '12:00',
+          },
+          'TH': {
+            from: '12:00',
+            to: '14:00',
+          },
+          'SU': {
+            from: '20:00',
+            to: '21:00',
+          },
         },
       },
-    },
-    {
-      name: 'ASTRID',
-      schedule: {
-        'MO': {
-          from: '10:00',
-          to: '12:00',
-        },
-        'TH': {
-          from: '12:00',
-          to: '14:00',
-        },
-        'SU': {
-          from: '20:00',
-          to: '21:00',
-        },
-      },
-    },
-  ]);
+    });
 });
 
 test('Test hour range coincidence', () => {
