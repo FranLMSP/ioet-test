9d89b059c1747dd97aba2648b224a277bfa6e667
Franco Colmenarez <info@francoacg.com>
2022-01-24 20:05:48 -0500

    Define final test

--- schedule-coincidences.test.js
+++ schedule-coincidences.test.js
@@ -19,3 +19,27 @@ test('Decode employee schedule text decode', () => {
     },
   });
 });
+
+test('Test process the text input', () => {
+  expect(processEmployeesText(
+`RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00- 21:00
+ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00
+ANDRES=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00
+`
+  )).toEqual(
+`
+ASTRID-RENE: 2
+ASTRID-ANDRES: 3
+RENE-ANDRES: 2
+`
+  );
+
+  expect(processEmployeesText(
+`RENE=MO10:15-12:00,TU10:00-12:00,TH13:00-13:15,SA14:00-18:00,SU20:00-21:00
+ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00
+`
+  )).toEqual(
+`RENE-ASTRID: 3
+`
+  );
+});
