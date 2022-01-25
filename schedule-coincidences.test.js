3fa84268afb1aa96e999fb7574a30a7eb2a4f818
Franco Colmenarez <info@francoacg.com>
2022-01-24 19:58:42 -0500

    Define test for decode employee schedule

--- schedule-coincidences.test.js
+++ schedule-coincidences.test.js
@@ -1,3 +1,21 @@
-test('sample test', () => {
-  expect(1 + 2).toBe(3);
+const { getEmployeeSchedule } = require('./schedule-coincidences');
+
+test('Decode employee schedule text decode', () => {
+  expect(getEmployeeSchedule('MO10:15-12:00')).toEqual({
+    'MO': {
+      from: {hour: 10, min: 15},
+      to: {hour: 12, min: 0},
+    },
+  });
+
+  expect(getEmployeeSchedule('MO10:15-12:00,TU10:00-12:00')).toEqual({
+    'MO': {
+      from: {hour: 10, min: 15},
+      to: {hour: 12, min: 0},
+    },
+    'TU': {
+      from: {hour: 10, min: 0},
+      to: {hour: 12, min: 0},
+    },
+  });
 });
